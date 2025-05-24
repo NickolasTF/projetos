e939f0a743d726359261d409793be4e34f08cb32
Nickolas Torres Faria <nick.tf@hotmail.com>
2025-05-24 19:46:32 -0300

    Começo do projeto XADREZ, criei o tabuleiro e o estilizei e salvei as imagens das peças

---
+++ xadrez/scripts/colorBoard.js
@@ -0,0 +1,21 @@
+/*CORES DO TABULEIRO 
+ #B58863 
+ #F0D9B5 
+*/
+let columns = 'abcdefgh'
+
+for (let i = 0; i < 8; i++) {
+
+  for(let j = 0; j < 8; j++) {
+
+   if ((i+j) % 2 === 0) {
+    document.querySelector(`.${columns[i]}${j+1}`).style.backgroundColor = '#B58863'
+   } else {
+    document.querySelector(`.${columns[i]}${j+1}`).style.backgroundColor = '#F0D9B5'
+   }
+
+  }
+
+}
+
+//me gerou problemas, pois cria estilos inline, q n da pra sobreescrever dps com CSS, vou resolver isso usando em CSS o nth-child
