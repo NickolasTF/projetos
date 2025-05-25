d4ff6bc6da343ba6dba678b3b01db0d610d2bb06
Nickolas Torres Faria <nick.tf@hotmail.com>
2025-05-24 22:40:55 -0300

    Primeira logica para a movimentação das peças

--- xadrez/scripts/main.js
+++ xadrez/scripts/main.js
@@ -1,7 +1,45 @@
 
 
+// primeiro eu faço uma matriz com todas as peças, assim eu associo essa matriz com o tabuleiro(square-ij) dps faço o JS pra cada movimento de peça, vendo onde ela ta e onde ela pode ir dps eh so ver se ja tem uma peça la e fazer a relação
+
+const columns = 'abcdefgh';
+
+const board1 = [
+  ['a8','b8','c8','d8','e8','f8','g8','h8'],
+  ['a7','b7','c7','d7','e7','f7','g7','h7'],
+  ['a6','b6','c6','d6','e6','f6','g6','h6'],
+  ['a5','b5','c5','d5','e5','f5','g5','h5'],
+  ['a4','b4','c4','d4','e4','f4','g4','h4'],
+  ['a3','b3','c3','d3','e3','f3','g3','h3'],
+  ['a2','b2','c2','d2','e2','f2','g2','h2'],
+  ['a1','b1','c1','d1','e1','f1','g1','h1']
+];
+
+const board2 = [
+  ['18','28','38','48','58','68','78','88'],
+  ['17','27','37','47','57','67','77','87'],
+  ['16','26','36','46','56','66','76','86'],
+  ['15','25','35','45','55','65','75','85'],
+  ['14','24','34','44','54','64','74','84'],
+  ['13','23','33','43','53','63','73','83'],
+  ['12','22','32','42','52','62','72','82'],
+  ['11','21','31','41','51','61','71','81']
+];
+
+// maiusculas -> black, minusculas -> white
+const board = [
+  ["R","N","B","Q","K","B","N","R"],//8
+  ["P","P","P","P","P","P","P","P"],//7
+  ["","","","","","","",""],//6
+  ["","","","","","","",""],//5
+  ["","","","","","","",""],//4
+  ["","","","","","","",""],//3
+  ["p","p","p","p","p","p","p","p"],//2
+  ["r","n","b","q","k","b","n","r"]//1
+];
 
 
 
-
-
+// PRIMEIRO PASSO COLOCAR AS CASAS COM OS NOMES DAS PEÇAS (R -> ROOK PRETO, r -> ROOK BRANCO)
+// DPS FAZER O MOVIMENTO DELAS COM EXEMPLO COLOCA UMA MATRIZ COM A PEÇA TESTE NO CENTRO E VAI TESTANDO ALGUNS CASOS E ESCREVENDO O CODIGO
+// POR FIM INCORPORAR AS IMAGENS DE CADA PEÇA  
---
+++ xadrez/scripts/moves.js
@@ -0,0 +1,80 @@
+function move(piece, square) {
+//on click() = "move('N', `${qual quadrado ela esta}`)" daqui eu verifico q q eu posso fazer com essa peça, (q cor ela eh) o q vou passar pra a função peçaMove() e (a partir desse quadrado pra onde ela pode ir)retorno da função peçaMove()
+
+
+
+
+/*
+
+eu associo cada peça a um par da matriz ixj board
+on click() eu passo a string associada como parametro pra k e daqui eu chamo a função correspondente ao movimento dessa peça
+
+*/
+
+//e ai na função peçaMove eu retorno alguma coisa e aqui eu gero o HTML com o innnerHTML(da div pra qual a peça vai) = <img da peça> ou melhor posso fazer o click dar um highlight nas possiveis casas de movimentação, caso clique em alguma delas a peça vai pra la 
+
+}
+
+
+
+function rookMove() {
+
+
+
+
+
+
+
+}
+
+
+
+function knightMove() {
+
+//primeiro verifico se eh branca ou preta(posso receber como parametro da função move, q vai me trazer pra k e vai dizer se eh B || W), pra q no final eu possa comparar com qual time ela pertence e verificar se o movimento eh valido ou n
+
+/* 
+primeiramente tenho q fazer um movimento valido 
+
+dado tal casa q essa peça está eu retorno quais possiveis casas ela pode ir e faço os proximos passos
+
+por exemplo: cavalo(pq ele pula fica mais facil 1°) anda uma na horizontal e 3 na vertical || uma na vertical e 3 na horizontal
+
+dps tenho q ver se vai cair em uma casa com uma peça aliada se sim, n pode, se n substitui a peça inimiga por essa 
+
+*/
+
+
+
+
+
+
+
+
+}
+
+
+
+function bishopMove() {
+
+}
+
+
+
+function queenMove() {
+
+}
+
+
+
+function kingMove() {
+
+}
+
+
+
+function pawnMove() {
+
+}
+
+
